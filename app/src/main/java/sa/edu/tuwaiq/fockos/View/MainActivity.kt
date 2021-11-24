de3cb2f2f604e4c94463ab0ff4c2e9c77ea54f2b
Muhannad Alghamdi <MuhannadAlghamdi@outlook.com>
2021-11-24 11:16:39 +0300

    first commit

---
+++ app/src/main/java/sa/edu/tuwaiq/fockos/View/MainActivity.kt
@@ -0,0 +1,12 @@
+package sa.edu.tuwaiq.fockos.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sa.edu.tuwaiq.fockos.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
